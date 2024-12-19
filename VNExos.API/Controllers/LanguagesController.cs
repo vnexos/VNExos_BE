3b04d9d83ce4758304958c81de39d1810ef1c6b4
Ilnung Terl anofp Drohelviaskhf <vnitd.owner@gmail.com>
2024-12-19 08:01:27 +0700

    Add common for controllers

--- VNExos.API/Controllers/LanguagesController.cs
+++ VNExos.API/Controllers/LanguagesController.cs
@@ -1,4 +1,5 @@
-﻿using MediatR;
+﻿using Azure.Core;
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using VNExos.API.Helpers;
 using VNExos.Application.Languages.Commands.CreateLanguage;
@@ -12,73 +13,35 @@ namespace VNExos.API.Controllers;
 
 [Route("api/[controller]")]
 [ApiController]
-public class LanguagesController (IMediator mediator) : ControllerBase
+public class LanguagesController (IMediator mediator) : CommonController<LanguageDto>
 {
     [HttpGet]
     public async Task<IActionResult> GetAll([FromQuery] GetAllLanguagesQuery request)
     {
-        try
-        {
-            var res = await mediator.Send(request);
-            return ApiResponse<ICollection<LanguageDto>>.CreateOk(res);
-        } catch (Exception ex)
-        {
-            return ApiResponse<string>.CreateInternalError(ex.Message);
-        }
+        return await Execute(request);
     }
 
     [HttpPost]
     public async Task<IActionResult> Create([FromBody] CreateLanguageCommand request)
     {
-        try
-        {
-            var res = await mediator.Send(request);
-            return ApiResponse<LanguageDto>.CreateOk(res);
-        } catch(Exception ex)
-        {
-            return ApiResponse<string>.CreateInternalError(ex.Message);
-        }
+        return await Execute(request);
     }
 
     [HttpPatch("{id:guid}")]
     public async Task<IActionResult> Patch([FromRoute] Guid id, [FromBody] UpdateLanguageCommand request)
     {
-        request.Id = id;
-        try
-        {
-            var res = await mediator.Send(request);
-            return ApiResponse<LanguageDto>.CreateOk(res);
-        } catch(Exception ex)
-        {
-            return ApiResponse<string>.CreateInternalError(ex.Message);
-        }
+        return await Execute(request);
     }
 
     [HttpGet("{code}")]
     public async Task<IActionResult> GetByCode([FromRoute] string code)
     {
-        var request = new GetLanguageByCodeQuery { Code = code };
-        try
-        {
-            var res = await mediator.Send(request);
-            return ApiResponse<LanguageDto>.CreateOk(res);
-        } catch (Exception ex)
-        {
-            return ApiResponse<string>.CreateInternalError(ex.Message);
-        }
+        return await Execute(new GetLanguageByCodeQuery { Code = code });
     }
 
     [HttpDelete("{id:guid}")]
     public async Task<IActionResult> Delete([FromRoute] Guid id)
     {
-        var request = new DeleteLanguageCommand { Id = id };
-        try
-        {
-            var res = await mediator.Send(request);
-            return ApiResponse<LanguageDto>.CreateOk(res);
-        } catch(Exception ex)
-        {
-            return ApiResponse<string>.CreateInternalError(ex.Message);
-        }
+        return await Execute(new DeleteLanguageCommand { Id = id });
     }
 }
---
+++ VNExos.API/Helpers/CommonController.cs
@@ -0,0 +1,36 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using VNExos.Common.DataTransferObject;
+using VNExos.Common.Transferer;
+
+namespace VNExos.API.Helpers;
+
+public class CommonController<TDto>(IMediator mediator) : ControllerBase
+    where TDto : CommonDto
+{
+    protected async Task<IActionResult> Execute<TRequest>(TRequest request)
+        where TRequest : CommonTransferer<TDto>
+    {
+        try
+        {
+            var res = await mediator.Send(request);
+            return ApiResponse<object>.CreateOk(res);
+        } catch (Exception ex)
+        {
+            return ApiResponse<string>.CreateInternalError(ex.Message);
+        }
+    }
+
+    protected async Task<IActionResult> Execute(CommonListTransferer<TDto> request)
+    {
+        try
+        {
+            var res = await mediator.Send(request);
+            return ApiResponse<object>.CreateOk(res);
+        }
+        catch (Exception ex)
+        {
+            return ApiResponse<string>.CreateInternalError(ex.Message);
+        }
+    }
+}
